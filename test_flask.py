cde626299fc83a75b9eaec3557114be3f2078406
Nicholas Murimi <nicholasmurimi254@gmail.com>
2025-03-19 06:51:17 +0300

    Update test_flask.py

--- test_flask.py
+++ test_flask.py
@@ -211,5 +211,4 @@ def cartoonify():
         return jsonify({"error": str(e)}), 500
     
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', 10000)
